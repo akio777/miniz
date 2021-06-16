142ce8ec5e05126f55f397bd10eb0d8e6874df0f
Chanutpong Promsin <chanutpong.promsin@gmail.com>
2021-06-16 10:48:11 +0700

    clear something

--- react/src/components/Header/index.jsx
+++ react/src/components/Header/index.jsx
@@ -6,10 +6,7 @@ import Search from '../Search';
 export default function Header(){
     return(
         <div className="header">
-            <div className="menu"><FiMenu size={24}/></div>
-            <div className="logo">LOGOAPP</div>
-            <Search />
-            <div className="account">{"Account"}</div>
+            
         </div>
     )
 }
--- react/src/components/Search/index.jsx
+++ react/src/components/Search/index.jsx
@@ -11,20 +11,9 @@ export default function Search(props) {
   return (
     <>
     
-    <div className="SeachBox">
-      <input placeholder="ค้นหา" />
-      <div className="box">
-        <FiSearch className="button" />
-      </div>
+    <div>
+      
     </div>
-
-    {/* {show&&<div className="SeachBoxMobile">
-      <input placeholder="ค้นหา" onBlur={()=>setShow(false)} autoFocus/>
-      <div className="box">
-        <FiSearch className="button" />
-      </div>
-    </div>}
-    {!show&&<FiSearch className="single" onClick={()=>setShow(true)}/>} */}
 
 
     </>
