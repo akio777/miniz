2d2900779247617ea7267ad3f2df8e22d52f5f55
Chanutpong Promsin <chanutpong.promsin@gmail.com>
2021-07-11 01:37:45 +0700

    WTF : DOCKER & DATABASE/SQL

--- goechov2/database/database.go
+++ goechov2/database/database.go
@@ -11,17 +11,16 @@ var DB *sql.DB
 
 func ConnectingDatabase(user string, password string, port string, db_name string) {
 	DB_URL := `postgres://` + user +
-		`:` + password + `@localhost:` + port + `/` +
+		`:` + password + `@` + `database:5434` + `/` +
 		db_name + `?sslmode=disable`
 	db, err := sql.Open("postgres", DB_URL)
 	if err != nil {
 		log.Printf("Unable to connect database : %v\n", err)
-		panic(err)
 	}
 	err = db.Ping()
 	if err != nil {
 		log.Printf("database connection error : %v\n", err)
-		panic(err)
+		// 	panic(err)
 	}
 	DB = db
 }
